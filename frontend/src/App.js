3b863ddcbab8de9bfcf4a6d308cd1cac075d26cc
Leivaa21 <adrianleiva212@gmail.com>
2020-10-27 10:05:31 +0100

    Creamos la aplicacion de react

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css';
+
+import Navigation from './components/Navigation'
+import Noteslist from './components/Noteslist'
+import CreateUser from './components/CreateUser'
+import CreateNote from './components/CreateNote'
+function App() {
+  return (
+    <Router>
+      <Navigation/>
+
+      <Route path="/" exact component={Noteslist}/>
+      <Route path="/edit/:id" component={CreateNote}/>
+      <Route path="/create" component={CreateNote}/>
+      <Route path="/user" component={CreateUser}/>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Noteslist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Noteslist extends Component {
+    render() {
+        return (
+            <div>
+                Noteslist
+            </div>
+        )
+    }
+}
