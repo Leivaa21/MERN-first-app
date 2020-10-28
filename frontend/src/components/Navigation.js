c2d62dceb8faaa444de643fbe800185a2ff7cfb7
Leivaa21 <adrianleiva212@gmail.com>
2020-10-28 11:49:08 +0100

    Navbar añadida y empezada la pestaña de usuarios basica

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
 export default class Navigation extends Component {
     render() {
         return (
-            <div>
-                Navigation
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <Link className="navbar-brand" to="/">Notes</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <Link className="nav-link active" to="/">Notes<span class="sr-only">(current)</span></Link>
+                            <Link className="nav-link" to="/create">New Note</Link>
+                        </div>
+                        <div className="navbar-nav ml-auto">
+                            <Link className="nav-link" to="/user">Register</Link>
+                        </div>
+                    </div>
+            </nav>
         )
     }
 }
